940f13c04de92e61b376200c4a4a126cec74455a
Andrea Stagi <stagi.andrea@gmail.com>
2018-01-03 21:42:41 +0100

    Add pystache

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import falcon
+import pystache
+
 
 def load_template(filename, context={}):
     f = open('public/templates/' + filename)
-    return f.read()
+    content = f.read()
+    return pystache.render(content, context)
 
 class MainResource:
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.content_type = 'text/html'
-        resp.body = load_template('index.html')
+        context = {
+            'title' : 'SARRY'
+        }
+        resp.body = load_template('index.html', context)
 
 class QuoteResource:
     def on_get(self, req, resp):
